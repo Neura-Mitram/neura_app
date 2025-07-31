1d737b18b8743a1778c51332598cbdc3799c24ec
By Shiladitya Mallick <info.shiladityamallick@gmail.com>
2025-07-31 17:21:42 +0000

    🔄 Sync API router

--- lib/services/api_base.dart
+++ lib/services/api_base.dart
@@ -3,4 +3,4 @@
 // const String Baseurl = "https://byshiladityamallick-neura-smart-assistant.hf.space";
 
 /// Dev Backend base URL
-const String Baseurl = "https://friendly-umbrella-v65p5pv74v9gfxp7v-7860.app.github.dev/";
+const String Baseurl = "https://friendly-umbrella-v65p5pv74v9gfxp7v-7860.app.github.dev";
