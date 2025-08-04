0b0029b5108390631c4bfaf10856e85c32c42c90
By Shiladitya Mallick <info.shiladityamallick@gmail.com>
2025-08-04 10:27:32 +0000

    Fiexed Lag

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:neura_app/screens/insights_screen.dart';
-import 'package:neura_app/screens/manage_subscription_screen.dart';
-import 'package:neura_app/screens/sos_contact_screen.dart';
+import 'screens/insights_screen.dart';
+import 'screens/manage_subscription_screen.dart';
+import 'screens/sos_contact_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'screens/login_screen.dart';
 import 'screens/chat_screen.dart';
